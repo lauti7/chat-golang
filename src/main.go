1bd67afc0efb0c829e3f1fdbacbcd4aaa7846919
lauti7 <laugerbustos@hotmail.com>
2020-06-30 18:56:56 -0300

    added gin tonic frameowrk

--- src/main.go
+++ src/main.go
@@ -1,37 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"net/http"
+	"./controllers"
 )
 
 var upgrader = websocket.Upgrader{}
 
 func main() {
 
-	clientManager := ClientManager{
-		Clients:    make(map[Client]bool),
-		Broadcast:  make(chan Message),
-		Typing:     make(chan Message),
-		Register:   make(chan Client),
-		Unregister: make(chan Client),
+	// clientManager := ClientManager{
+	// 	Clients:    make(map[Client]bool),
+	// 	Broadcast:  make(chan Message),
+	// 	Typing:     make(chan Message),
+	// 	Register:   make(chan Client),
+	// 	Unregister: make(chan Client),
+	// }
+
+	//Channels start waiting for receiving
+	// go clientManager.run()
+
+	server := gin.Default()
+	server.Use(CORSMiddleware())
+
+	api := server.Group("/api")
+	{
+		api.GET("/", func (c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "API IS ALIVE",
+			})
+		})
+
+		api.GET("/users", controllers.GetUsers)
+		api.POST("/users", controllers.CreateUser)
 	}
 
-	fs := http.FileServer(http.Dir("../chatapp/build"))
+	// server.GET("/ws", clientManager.handleConnections)
 
-	http.Handle("/chat", fs)
 
-	go clientManager.run()
+	server.Run()
 
-	http.HandleFunc("/ws", clientManager.handleConnections)
+}
 
-	err := http.ListenAndServe(":8000", nil)
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
-	if err != nil {
-		fmt.Println("ListenAndServe: ", err)
-	}
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
 
-	fmt.Println("http server started on :8000")
-
+        c.Next()
+    }
 }
