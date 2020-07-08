458633bc56da5d65f8842671a5e1572afe3920b3
lauti7 <laugerbustos@hotmail.com>
2020-07-08 20:12:17 -0300

    http handler for new message

--- src/api/message/message_controller.go
+++ src/api/message/message_controller.go
@@ -1,1 +1,24 @@
 package message
+
+import (
+	messageModel "../models"
+	"github.com/gin-gonic/gin"
+)
+
+func NewMessage(c *gin.Context) {
+
+  var msg messageModel.Message
+
+  err := c.ShouldBindJSON(&msg)
+
+  if err != nil {
+    c.JSON(400, gin.H{
+      "err": "Bad Request."
+    })
+  }
+
+  msg.CreateMessage()
+
+	//Si el usario que recibe esta conectado enviar websocket, si no devuelve 200 status:true
+
+}
