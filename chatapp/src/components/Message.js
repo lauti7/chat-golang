fa89a4cf1adabe1484dee0785141ef841d97b5da
lauti7 <laugerbustos@hotmail.com>
2020-07-11 10:27:33 -0300

    changes for new Message model

--- chatapp/src/components/Message.js
+++ chatapp/src/components/Message.js
@@ -9,10 +9,10 @@ const Message = ({msg}) => {
       <Col md="12">
         <Card body inverse style={{ marginTop: "4px",backgroundColor: '#333', borderColor: '#333', height: "100px" }}>
           <div className="d-flex">
-            <p className="px-1">{msg.sender.userName}</p>
-            <p className="px-1">{msg.timestamp}</p>
+            <p className="px-1">{msg.user_id}</p>
+            <p className="px-1">{msg.created_at}</p>
           </div>
-          <p className="px-1">{msg.message}</p>
+          <p className="px-1">{msg.content}</p>
         </Card>
       </Col>
     </Row>
