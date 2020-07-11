35feae7c90b8aea7266bcf5c9cecf0269a4f0db5
lauti7 <laugerbustos@hotmail.com>
2020-07-11 10:20:29 -0300

    reorganized websocket code

--- src/websockets.go
+++ src/websockets.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/websocket"
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"./api/models"
 )
 
 var upgrader = websocket.Upgrader{}
@@ -17,115 +19,169 @@ type UsersManager struct {
 
 type BroadcastMessage struct {
 	ID        string     `json:"id,omitempty"`
+	ChatID    uint        `json:"chat_id"`
 	Sender    OnlineUser `json:"sender"`
+	ReceiverUserID uint `json:"receiver_user_id"`
 	Content   string     `json:"content,omitempty"`
 	Type      string     `json:"type"`
 	Timestamp string     `json:"timestamp,omitempty"`
 }
 
 type OnlineUser struct {
-	SocketID      *websocket.Conn       `json:"socketId,omitempty"`
+	SocketID      *websocket.Conn       `json:"socket_id,omitempty"`
 	ChatChannel   chan BroadcastMessage `json:"omitempty"`
 	TypingChannel chan BroadcastMessage `json:"omitempty"`
 	Username      string                `json:"user_name"`
-	UserID        uint                  `json:"user_id"`
+	UserID        uint                  `json:"user_id" form:"user_id"`
 }
 
-func (manager *UsersManager) handleWS(w http.ResponseWriter, r *http.Request) {
+type OnlineUserMessage struct {
+	Type   string `json:"type"`
+	UserID uint `json:"user_id"`
+}
 
-	upgrader.CheckOrigin = func(r c.Request) bool { return true }
-	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+func (manager *UsersManager) handleWS(w http.ResponseWriter, r *http.Request, authUser interface{}) {
+
+	onlineUser := authUser.(OnlineUser)
+
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	ws, err := upgrader.Upgrade(w, r, nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(ws)
+	onlineUser.SocketID = ws
 
 	defer ws.Close()
 
-	// client := Client{
-	// 	SocketID: ws,
-	// }
-	//
-	// fmt.Printf("Client: %v", client)
-	//
-	// socketerr := ws.ReadJSON(&client)
-	//
-	// fmt.Printf("Client: %v", client)
-	//
-	// if socketerr != nil {
-	// 	log.Printf("error: %v", err)
-	// }
-	//
-	// manager.RegisterChannel <- client
-	//
-	// for {
-	// 	fmt.Println("Recorriendo handleConnections")
-	// 	var msg Message
-	//
-	// 	fmt.Println("almost reading")
-	// 	err := ws.ReadJSON(&msg)
-	//
-	// 	msg.Sender = client
-	//
-	// 	if err != nil {
-	// 		log.Printf("error: %v", err)
-	// 		delete(manager.Clients, client)
-	// 		break
-	// 	}
-	// 	fmt.Println("Message: %v", msg)
-	//
-	// 	switch msg.Type {
-	// 	case "broadcast":
-	// 		manager.Broadcast <- msg
-	// 	case "typing":
-	// 		manager.Typing <- msg
-	// 	}
-	// }
-	//
-	// manager.UnregisterChannel <- client
-	//
-	// fmt.Println("Out handleConnections")
+	manager.RegisterChannel <- onlineUser
+
+	go onlineUser.chat(manager)
+
+	for {
+		fmt.Println("::handleConnections")
+		var msg BroadcastMessage
+
+		err := ws.ReadJSON(&msg)
+
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(manager.OnlineUsers, onlineUser.UserID)
+			break
+		}
+
+		msg.Sender = onlineUser
+
+		receiverUser, online := manager.OnlineUsers[msg.ReceiverUserID]
+
+		if online {
+			switch msg.Type {
+			case "chat":
+				receiverUser.ChatChannel <- msg
+			case "typing":
+				receiverUser.TypingChannel <- msg
+			}
+		}
+
+		newMessage := models.Message{
+			ChatID: msg.ChatID,
+			UserID: msg.Sender.UserID,
+			Content: msg.Content,
+			ReceiverID:
+			msg.ReceiverUserID,
+		}
+		newMessage.CreateMessage()
+		continue
+	}
+	manager.UnregisterChannel <- onlineUser
 
 }
 
-func (manager *UsersManager) run() {
+func (manager *UsersManager) registration() {
 	for {
 		select {
-		case client := <-manager.RegisterChannel:
-			manager.Clients[client] = true
-			newMsg := Message{
-				Sender:  client,
-				Message: "",
-				Type:    "join",
+		case user := <-manager.RegisterChannel:
+			manager.OnlineUsers[user.UserID] = user
+			u := OnlineUserMessage{
+				UserID: user.UserID,
+				Type: "new_user_online",
 			}
-			manager.send(newMsg)
-		case client := <-manager.UnregisterChannel:
-			manager.Clients[client] = false
-			newMsg := Message{
-				Sender:  client,
-				Message: "",
-				Type:    "leave",
+			manager.newUserOnline(u)
+		case user := <-manager.UnregisterChannel:
+			manager.OnlineUsers[user.UserID] = user
+			u := OnlineUserMessage{
+				UserID: user.UserID,
+				Type: "new_user_offline",
 			}
-			manager.send(newMsg)
-		// case msg := <-manager.Broadcast:
-		// 	manager.send(msg)
-		// case msg := <-manager.Typing:
-		// 	manager.send(msg)
+			manager.newUserOnline(u)
 		}
 	}
 }
 
-func (manager *UsersManager) send(msg Message) {
-	for client := range manager.Clients {
-		if client.SocketID != msg.Sender.SocketID {
-			err := client.SocketID.WriteJSON(msg)
+func (user *OnlineUser) chat(manager *UsersManager) {
+	for {
+		select {
+		case msg := <-user.ChatChannel:
+			user.receiveMsg(msg, manager)
+		case msg := <-user.TypingChannel:
+			user.receiveMsg(msg, manager)
+		}
+	}
+}
+
+func (user *OnlineUser) receiveMsg(msg BroadcastMessage, manager *UsersManager) {
+
+	err := user.SocketID.WriteJSON(msg)
+
+	if err != nil {
+		log.Printf("Error Routine: %v", err)
+		user.SocketID.Close()
+		delete(manager.OnlineUsers, user.UserID)
+	}
+}
+
+func (manager *UsersManager) newUserOnline(u OnlineUserMessage) {
+	for _, user := range manager.OnlineUsers {
+		if user.UserID != u.UserID {
+			err := user.SocketID.WriteJSON(u)
 			if err != nil {
 				log.Printf("Error Routine: %v", err)
-				client.SocketID.Close()
-				delete(manager.Clients, client)
+				user.SocketID.Close()
+				delete(manager.OnlineUsers, user.UserID)
 			}
 		}
 	}
 }
+
+
+func checkAndFindUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var onlineUser OnlineUser
+
+		c.Bind(&onlineUser)
+
+		username := models.GetUsername(onlineUser.UserID)
+
+		onlineUser.Username = username
+		onlineUser.ChatChannel = make(chan BroadcastMessage)
+		onlineUser.TypingChannel = make(chan BroadcastMessage)
+
+		c.Set("user", onlineUser)
+		c.Next()
+	}
+}
+
+//
+// func (manager *UsersManager) send(msg Message) {
+// 	for client := range manager.Clients {
+// 		if client.SocketID != msg.Sender.SocketID {
+// 			err := client.SocketID.WriteJSON(msg)
+// 			if err != nil {
+// 				log.Printf("Error Routine: %v", err)
+// 				client.SocketID.Close()
+// 				delete(manager.Clients, client)
+// 			}
+// 		}
+// 	}
+// }
