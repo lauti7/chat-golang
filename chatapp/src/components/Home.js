9bf55ab4f5b500b24f5ed70aefa8d084904a774a
lauti7 <laugerbustos@hotmail.com>
2020-07-11 10:29:18 -0300

    added receive message from websocket function

--- chatapp/src/components/Home.js
+++ chatapp/src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {useAuth} from '../auth'
 import {Container, Row, Col} from 'reactstrap'
 import Users from './Users'
@@ -7,6 +7,35 @@ import OneToOneChat from './OneToOneChat'
 const Home = () => {
 
   const {state, dispatch} = useAuth()
+
+  const receiveBroadcastedMsg = e => {
+    let msg = JSON.parse(e.data)
+    console.log(msg);
+    switch (msg.type) {
+      case "new_user_online":
+        dispatch({
+          type:"UPDATEUSERS",
+          newUser: msg.user_id,
+          status: 'on'
+        })
+        break
+      case "new_user_offline":
+        dispatch({
+          type: "UPDATEUSERS",
+          newUser: msg.user_id,
+          status: 'off'
+        })
+      default:
+        console.log("no type");
+    }
+  }
+
+  useEffect(() => {
+    if (state.ws) {
+      state.ws.addEventListener('message', receiveBroadcastedMsg)
+    }
+    console.log(state.users);
+  }, [])
 
   return (
     <Container fluid>
